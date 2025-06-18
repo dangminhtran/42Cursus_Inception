84aac136715f2e455e9ce9f3c67b6c05e6d18f93
ybourdea <dayota@gmail.com>
2025-06-18 12:53:24 +0200

    voilà mon mariadb ne fonctionne pas encore mais tu as le template pour wordpress

--- srcs/requirements/mariadb/conf/script.sh
+++ srcs/requirements/mariadb/conf/script.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
-set -e
+# Initialiser la base de données si elle n'existe pas déjà
+if [ ! -d "/var/lib/mysql/$MYSQL_DATABASE" ]; then
 
-service mariadb start
-sleep 5
+  : "${MYSQL_DATABASE:?Environment variable MYSQL_DATABASE not set}"
+  : "${MYSQL_USER:?Environment variable MYSQL_USER not set}"
+  : "${MYSQL_PASSWORD:?Environment variable MYSQL_PASSWORD not set}"
 
-# Ensure required variables are set
-: "${MYSQL_DATABASE:?Environment variable MYSQL_DATABASE not set}"
-: "${MYSQL_USER:?Environment variable MYSQL_USER not set}"
-: "${MYSQL_PASSWORD:?Environment variable MYSQL_PASSWORD not set}"
+    # Démarrer le serveur MySQL
+    service mariadb start
 
-mariadb -e "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\`;"
-mariadb -e "CREATE USER IF NOT EXISTS \`$MYSQL_USER\`@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-mariadb -e "GRANT ALL PRIVILEGES ON \`$MYSQL_DATABASE\`.* TO \`$MYSQL_USER\`@'%';"
-mariadb -e "FLUSH PRIVILEGES;"
+    # Créer la base de données et l'utilisateur
+    mariadb -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
+    mariadb -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+    mariadb -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
 
-if [ -n "$MYSQL_ROOT_PASSWORD" ]; then
-	mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+    if [ -n "$MYSQL_ROOT_PASSWORD" ]; then
+        # Changer le mot de passe root
+        mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+    fi
+
+    mariadb -e "FLUSH PRIVILEGES;"
+
+    # Arrêter le serveur MySQL
+    service mariadb stop
 fi
 
-mysqladmin -u root -p"$MYSQL_ROOT_PASSWORD" shutdown
-
-mysqld_safe --port=3306 --bind-address=0.0.0.0 --datadir='/var/lib/mysql'
+exec "$@"
--- srcs/requirements/wordpress/conf/php.sh
+++ srcs/requirements/wordpress/conf/php.sh
@@ -1,15 +1,43 @@
 #!/bin/sh
 
-if ! grep -q "listen = 0.0.0.0:9000" /etc/php/7.4/fpm/pool.d/www.conf; then
-	echo "listen = 0.0.0.0:9000" >>/etc/php/7.4/fpm/pool.d/www.conf
+php_version=$(php -v | head -n 1 | grep -oP 'PHP \K[0-9]+\.[0-9]+')
+
+if ! grep -q "listen = 0.0.0.0:9000" /etc/php/"$php_version"/fpm/pool.d/www.conf; then
+	echo "listen = 0.0.0.0:9000" >>/etc/php/"$php_version"/fpm/pool.d/www.conf
 fi
 
-sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
-sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
-sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
-sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
-cp /var/www/wordpress/wp-config-sample.php /var/www/wordpress/wp-config.php
+: "${MYSQL_HOST:?Environment variable MYSQL_HOST not set}"
+: "${MYSQL_DATABASE:?Environment variable MYSQL_DATABASE not set}"
+: "${MYSQL_USER:?Environment variable MYSQL_USER not set}"
+: "${MYSQL_PASSWORD:?Environment variable MYSQL_PASSWORD not set}"
+
+generate_random_string() {
+    cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 20 | head -n 1
+}
+
+cat <<EOT >> wp-config.php
+// based on https://developer.wordpress.org/advanced-administration/wordpress/wp-config/
+define( 'DB_NAME', '$MYSQL_DATABASE' );
+define( 'DB_USER', '$MYSQL_USER' );
+define( 'DB_PASSWORD', '$MYSQL_PASSWORD' );
+define( 'DB_HOST', '$MYSQL_HOST' );
+
+define( 'AUTH_KEY',         '$(generate_random_string)');
+define( 'SECURE_AUTH_KEY',  '$(generate_random_string)');
+define( 'LOGGED_IN_KEY',    '$(generate_random_string)');
+define( 'NONCE_KEY',        '$(generate_random_string)');
+define( 'AUTH_SALT',        '$(generate_random_string)');
+define( 'SECURE_AUTH_SALT', '$(generate_random_string)');
+define( 'LOGGED_IN_SALT',   '$(generate_random_string)');
+define( 'NONCE_SALT',       '$(generate_random_string)');
+
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/' );
+}
+
+require_once ABSPATH . 'wp-settings.php';
+EOT
 
 chown -R www-data:www-data /var/www/wordpress
 
-exec php-fpm7.4 -F
+exec php-fpm"$php_version" -F
